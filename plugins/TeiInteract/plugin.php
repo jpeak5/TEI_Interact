cb149f46941f25e41dfaf96b729e7ebf7137eeac
Jason Peak <jpeak5@lsu.edu>
2012-08-01 20:51:42 -0500

    Adding my stub plugin for decorating tei elements with jQuery fun

---
+++ plugins/TeiInteract/plugin.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * TEI Interact is a plugin to provide hooks into the TEI markup of a document.
+ * 
+ * We intend to extend the functionality provided by the Scholar's Lab TeiDisplay plugin
+ * @package TeiInteract 
+ */
+define('TEI_INTERACT_TEST', 'this is only a test');
+
+/**
+ * 
+ * 
+ * This class is the main script for the plugin as desribed in 
+ * @link http://omeka.org/codex/Plugin_Writing_Best_Practices The Omeka Plugins-writing best practices
+ */
+class TeiInteract extends Omeka_Plugin_Abstract {
+
+    /**
+     * hooks array
+     * The hooks that you declare you are using in the $_hooks array must have a corresponding public method of the form hook{Hookname} as above. 
+     * @link http://omeka.org/codex/Plugin_Writing_Best_Practices Plugins Best-practices
+     * @var string[] the set of hooks that this plugin uses
+     */
+    protected $_hooks = array('install','initialize', 'public_theme_header', 'public_theme_body');
+    
+    
+    public function hookInstall() {
+        //do your install work here
+    }
+
+    public function hookInitialize() {
+        debug('init just ran');
+    }
+
+    /**
+     *
+     * @param type $request 
+     */
+    public function hookPublicThemeHeader($request) {
+
+        $item = get_current_item();
+        
+//        echo $item->id;
+    }
+
+    /**
+     *
+     * @param type $request 
+     */
+    public function hookPublicThemeBody($request) {
+
+    }
+
+    public function addStuff(){
+        echo js('teiInteract');
+        debug('addStuff just ran');
+    }
+}
+
+$teiInteract = new TeiInteract();
+$teiInteract->setUp();
+
---
+++ plugins/TeiInteract/views/public/javascripts/teiInteract.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("p").hover(
+        function (event) {
+            var $target = event.target;
+            $(this).append($("<span class=\"green\">"+event.pageX+", "+event.pageY+" - "+$target.valueOf()+"</span>"));
+        }, 
+        function () {
+            $(this).find("span:last").remove();
+        }
+        );
+
+});
+
+
+
