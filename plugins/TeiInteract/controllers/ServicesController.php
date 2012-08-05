9814425f63d2e8c601ad52d487098759449af75c
Jason Peak <jpeak5@lsu.edu>
2012-08-05 09:19:55 -0500

    adding a Services controller to return an AJAX view to the .load js method

---
+++ plugins/TeiInteract/controllers/ServicesController.php
@@ -0,0 +1,11 @@
+<?php
+
+class TeiInteract_ServicesController extends Omeka_Controller_Action {
+    
+    
+    public function namesAction(){
+        $this->view->hello = "Hello World!";
+    }
+    
+}
+?>
