60e661b6a4a5e757d0ee024976de31ac740133d7
Jason Peak <jpeak5@lsu.edu>
2012-08-05 10:10:51 -0500

    getting rid of the old mouse-tracking stuff

--- plugins/TeiInteract/views/public/javascripts/teiInteract.js
+++ plugins/TeiInteract/views/public/javascripts/teiInteract.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $(".name").hover(
         function (event) {
-            var $target = event.target;
+//            var $target = event.target;
             $("#thing").load("http://apc.local/tei-interact/services/names");
-            $(this).append($("<span class=\"green\">"+event.pageX+", "+event.pageY+" - "+$target.valueOf()+"</span>"));
+//            $(this).append($("<span class=\"green\">"+event.pageX+", "+event.pageY+" - "+$target.valueOf()+"</span>"));
         }, 
         function () {
-            $(this).find("span:last").remove();
+//            $(this).find("span:last").remove();
             $("#thing").html("");
         }
         );
