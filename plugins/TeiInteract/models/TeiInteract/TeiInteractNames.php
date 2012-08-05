37b7a77a13585083d69fbb09130dac5cde963080
Jason Peak <jpeak5@lsu.edu>
2012-08-05 10:19:09 -0500

    beginning of a model and controller for inserting name occurrences to the db...

---
+++ plugins/TeiInteract/models/TeiInteract/TeiInteractNames.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Description of TeiInteract_hasNames
+ *
+ * @author jpeak5
+ */
+class TeiInteractNames extends Omeka_Record{
+    
+    
+    public $id;
+    public $file_id;
+    public $type;
+    public $value;
+    /**
+     *
+     * @var boolean true for header
+     */
+    public $teiHeader; 
+    
+}
+
+?>
