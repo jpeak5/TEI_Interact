aca332b7507da4d2d9f49dd201ca9c315711d8fd
jpeak5 <jpeak5@Lennyland.(none)>
2012-08-06 22:19:54 -0500

    round trip AJAX request -> DB Query -> View response -> jQuery response consumption works Details need A LOT of work to be presentable...

--- plugins/TeiInteract/views/public/services/names.php
+++ plugins/TeiInteract/views/public/services/names.php
@@ -1,4 +1,23 @@
 <?php
 
-echo $hello;
+debug('begin view');
+
+$buffer = "";
+$thing = "<div class=\"thing-content\">";
+foreach ($occurrences as $file => $fileAttrs) {
+    print_r("$file");
+    $buffer.="<span class=\"thing-result\">File:".$file." - (".$fileAttrs['count'].")</span><br/>";
+    debug("view got result ".$file);
+}
+if(!strlen($buffer)>0){
+    $thing.="<div class=\"thing-struckout\">No Results Found</div>";
+}else{
+    $thing.= "<div class=\"thing-result-header\">results for ".$searchStr.":</div>";
+    $thing.="<div class=\"thing-results\">";
+    $thing.=$buffer;
+    $thing.="</div>";
+}
+$thing.="</div>";
+echo $thing;
+
 ?>
