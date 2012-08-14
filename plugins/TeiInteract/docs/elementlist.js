59a0a8098175cc3de8a6e86bdb0016dd2621309b
jpeak5 <jpeak5@Lennyland.(none)>
2012-08-13 19:55:06 -0500

    first version of docs...

---
+++ plugins/TeiInteract/docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","SimpleXMLElement"],["c","TeiInteract"],["c","TeiInteract_ConfigController"],["c","TeiInteract_ListController"],["c","TeiInteract_ServicesController"],["c","TeiInteract_TagsController"],["c","TeiInteractionsController"],["c","TeiInteractName"],["c","TeiInteractNameTable"],["c","Traversable"]];
