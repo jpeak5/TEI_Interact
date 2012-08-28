0257b099a47f42f8c69d2f9221e008aa610d9c18
jason peak <jp5@localhost>
2012-08-27 22:19:39 -0500

    theme snapshot

---
+++ themes/omeka-foundation/common/footer.php
@@ -0,0 +1,9 @@
+
+<div class="row">
+	<div id="footer" class="twelve columns">
+		<?php admin_plugin_footer(); ?>
+		<ul class="pagination">
+			<?php echo custom_public_nav_header(); ?>
+		</ul>
+	</div>
+</div>
