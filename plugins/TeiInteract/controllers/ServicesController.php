6e6c67926c09677900f5005c96f704cfcac0b8ea
jason peak <jp5@localhost>
2012-08-28 11:38:19 -0500

    connecting the dots; returning the hovered data to the THING

--- plugins/TeiInteract/controllers/ServicesController.php
+++ plugins/TeiInteract/controllers/ServicesController.php
@@ -9,7 +9,9 @@ class TeiInteract_ServicesController extends Omeka_Controller_Action {
     public function lookupAction(){
         $searchStr = teiInteract_normalizeText($this->getRequest()->getParam('hover'));
         debug(sprintf("user mouse is hovering over TEI tag with value %s", $searchStr));
-        $this->view->result = "hello world";
+        $el = $this->getRequest()->getParam('element');
+        $this->view->search = $searchStr;
+        $this->view->element = $el;
     }
 }
 
