7fbd2123e392eac33d8bc1e4d53606439ddef191
jason peak <jp5@localhost>
2012-08-29 00:58:32 -0500

    bare minimum for proof of concept THING. With this change, related item references, including image thumbnails

--- plugins/TeiInteract/views/public/services/lookup.php
+++ plugins/TeiInteract/views/public/services/lookup.php
@@ -9,9 +9,24 @@
 $buffer = "";
 $thing = "<div class=\"thing-content\">";
 
-    $buffer.="<span class=\"thing-result\">".$element.":".$search."</span><br/>";
+    $buffer.="<span class=\"thing-result\">"."</span><br/>";
 
-
+    $buffer.="<ul>";
+        foreach ($related as $rel => $kvPair) {
+            foreach($kvPair as $k => $v){
+                $img = "";
+                $buffer.= "<li>".$k." = ";
+                
+                if(item_has_files($v)){
+                    $img = item_square_thumbnail(array(), 0, $v);
+                    debug($img);
+                }
+                
+                $buffer.= $v->id.$img."</li>";
+            }
+        }
+    $buffer.="</ul>";
+    
 
 
 /**
@@ -20,9 +35,9 @@ $thing = "<div class=\"thing-content\">";
  * otherwise, insert the results inside the block
  */
 
-    $thing.="<div class=\"thing-struckout\">No Results Found</div>";
+//    $thing.="<div class=\"thing-struckout\">No Results Found</div>";
 
-    $thing.= "<div class=\"thing-result-header\">results for ".$search.":</div>";
+    $thing.= "<div class=\"thing-result-header\">results for ".$element.":".$search."<br/>type ".$type.":</div>";
     $thing.="<div class=\"thing-results\">";
     $thing.=$buffer;
     $thing.="</div>";
